70245094f22af79a0bb197f9c1349265979898e2
Niels <nielsrowinbik@users.noreply.github.com>
2019-11-01 17:55:02 +0100

    Quick fix

--- src/App.svelte
+++ src/App.svelte
@@ -7,9 +7,9 @@
         <div class="container mx-auto flex flex-col items-center">
             <h2>Source</h2>
             <label for="file-upload" class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center .cursor-pointer">
-    Custom Upload
-</label>
-<input id="file-upload" type="file" class="hidden" />
+                Custom Upload
+            </label>
+            <input id="file-upload" type="file" class="hidden" />
             <p>or</p>
             <textarea class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none leading-normal resize-none"></textarea>
         </div>
--- src/FileInput.svelte
+++
@@ -1,4 +0,0 @@
-<label for="file-upload" class="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center .cursor-pointer">
-    Custom Upload
-</label>
-<input id="file-upload" type="file"/>
